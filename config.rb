a5ec55429eec6c040df37ab1a8ef392cbec667a8
cyber <cyber@21212.com>
2014-01-04 22:09:49 -0200

    new region for camerasrj

--- config.rb
+++ config.rb
@@ -72,7 +72,7 @@ end
 
 activate :s3_sync do |s3_sync|
   s3_sync.bucket                     = 'www.camerasrj.com.br' # The name of the S3 bucket you are targetting. This is globally unique.
-  s3_sync.region                     = 'sa-east-1'     # The AWS region for your bucket.
+  s3_sync.region                     = 'us-east-1'     # The AWS region for your bucket.
   # set ENV vars:
   #  AWS_ACCESS_KEY_ID
   #  AWS_SECRET_ACCESS_KEY
