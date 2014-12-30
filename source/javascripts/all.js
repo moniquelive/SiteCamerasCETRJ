456848fd40e745ef3f74aa79994f7eba20908775
cyber <cyber@21212.com>
2014-12-30 14:53:46 -0200

    much less javascript

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,4 @@
 //=include lodash/dist/lodash.js
 //=include jquery/dist/jquery.js
 //=include jquery-smooth-scroll/jquery.smooth-scroll.js
-//=include angularjs/angular.js
-//=include angular-route/angular-route.js
 //=include js.js
