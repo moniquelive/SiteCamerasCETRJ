d725b5008b1a5f250504d9912ccd3dc498cafc38
cyber <cyber@21212.com>
2015-07-07 21:30:38 -0300

    adding google material design lite to the project

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,5 @@
 //=include lodash/dist/lodash.js
 //=include jquery/dist/jquery.js
 //=include jquery-smooth-scroll/jquery.smooth-scroll.js
+//=include material-design-lite/material.min.js
 //=include js.js
